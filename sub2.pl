0f9d0f2f0f3742d93833af8cf659c9870705163f
andyc1997 <45092498+andyc1997@users.noreply.github.com>
2019-02-16 22:48:51 +0800

    Add files via upload
    Encoding problem.

---
+++ sub2.pl
@@ -0,0 +1,28 @@
+use warnings;
+use strict;
+use WWW::Mechanize;
+
+sub main{
+    my $mech = WWW::Mechanize->new();
+    for my $i (1..32){
+        my $filepath = &getfile($i);
+        print $filepath, "\n";
+        my $tempfile = "C:\\Users\\user\\Desktop\\novel\\Novel$i.txt";
+        $mech->get($filepath, ':content_file' => $tempfile); #Encoding problem here.
+        #open("C:\\Users\\user\\Desktop\\novel\\Novel$i.txt", "<", \$text) or die "Failed!";
+    }
+}
+
+sub getfile{
+    my ($num) = @_;
+    if ($num < 10 and $num > 0){
+        $num = "00$num";
+    } elsif ($num < 100){
+        $num = "0$num";
+    } else {
+        $num = "$num";
+    }
+    my $filename = 'http://www.geocities.jp/louisng888jp/yuen/';
+    return "$filename$num.txt";
+}
+&main;
